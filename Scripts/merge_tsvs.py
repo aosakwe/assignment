679e5f8c9ffbe06faf628239b22d397fac4bf91c
Michelle Wang <tomichellewang@gmail.com>
2022-03-09 23:13:05 -0500

    fix issue with pandas method

--- Scripts/merge_tsvs.py
+++ Scripts/merge_tsvs.py
@@ -9,18 +9,20 @@ files = os.path.join(pattern)
 # list of files to merge
 files = glob.glob(files)
 
-fout=open("./Data/Labeling/merged_annot.tsv","a",encoding='cp850')   
-for idx,file in enumerate(files):
-	print(file)
-	if idx == 0: #skips header for every subsequent file
-		f = open(file,encoding='cp850')#.readlines()
-	else:
-		f = open(file,encoding='cp850').readlines()[1:]
-	for idx,line in enumerate(f):
-		fout.write(line)
-fout.close()
+# fout=open("./Data/Labeling/merged_annot.tsv","w",encoding='cp850')   
+# for idx,file in enumerate(files):
+# 	print(file)
+# 	if idx == 0: #skips header for every subsequent file
+# 		f = open(file,encoding='cp850')#.readlines()
+# 	else:
+# 		f = open(file,encoding='cp850').readlines()[1:]
+# 	for idx,line in enumerate(f):
+# 		if idx == 0:
+# 			print(line)
+# 		fout.write(line)
+# fout.close()
 
 # joining files with concat and read_csv
-#issue : gives NaN type for the annotation columns
-# df = pd.concat(map(lambda fs: pd.read_csv(fs, sep='\t'), files))
-# print(df)
+colnames = ['PMID', 'Title', 'Authors', 'Citation', 'First.Author', 'Journal.Book', 'Publication.Year', 'Create.Date', 'PMCID', 'NIHMS.ID', 'DOI', 'Data Available', 'Open Access', 'Code Available', 'Notes']
+df = pd.concat(map(lambda fs: pd.read_csv(fs, sep='\t', names=colnames, header=0), files))
+df.to_csv('./Data/Labeling/merged_annot.tsv', sep='\t', index=False)
